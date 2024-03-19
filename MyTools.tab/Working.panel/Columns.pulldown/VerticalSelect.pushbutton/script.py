a5cae84a66a543e0157cbb25d658f9d70dc1344b
justadrafter <161539151+justadrafter@users.noreply.github.com>
2024-03-19 12:02:13 +0800

    Added multicolumn input

--- MyTools.tab/Working.panel/Columns.pulldown/VerticalSelect.pushbutton/script.py
+++ MyTools.tab/Working.panel/Columns.pulldown/VerticalSelect.pushbutton/script.py
@@ -1,28 +1,56 @@
-from pyrevit import revit, DB
-from Autodesk.Revit.DB import LocationCurve, LocationPoint
+from pyrevit import revit, DB, forms, script
+from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory, XYZ
 
-doc = __revit__.ActiveUIDocument.Document
+doc = revit.doc
 
-def get_parameter_value_by_name(element, parameterName):
-	return element.LookupParameter(parameterName).AsValueString()
+def get_parameter_value_by_name(element, parameter_name):
+    """
+    Retrieves the value of a parameter from an element as a string.
+    
+    :param element: Revit element
+    :param parameter_name: Name of the parameter
+    :return: Parameter value as a string
+    """
+    return element.LookupParameter(parameter_name).AsValueString()
 
-def point_close(_pointA,_pointB):
-    return abs(_pointA.X-_pointB.X) < 2 and abs(_pointA.Y-_pointB.Y) < 2
+def is_point_close(point_a, point_b, tolerance=2):
+    """
+    Checks if two points are close to each other within a specified tolerance.
+    
+    :param point_a: First point (XYZ)
+    :param point_b: Second point (XYZ)
+    :param tolerance: Tolerance value (default: 2)
+    :return: True if points are close, False otherwise
+    """
+    return abs(point_a.X - point_b.X) < tolerance and abs(point_a.Y - point_b.Y) < tolerance
 
-def bbmid(_col):
-    _bb = _col.get_BoundingBox(None)
-    return (_bb.Max + _bb.Min)/2
+def get_bounding_box_center(element):
+    """
+    Calculates the center point of an element's bounding box.
+    
+    :param element: Revit element
+    :return: Center point of the bounding box (XYZ)
+    """
+    bounding_box = element.get_BoundingBox(None)
+    return (bounding_box.Max + bounding_box.Min) / 2
 
-selection = revit.get_selection()
+selected_elements = revit.get_selection()
 
-baselocation = bbmid(selection[0])
+if not selected_elements:
+    forms.alert("Please select at least one element.")
+    script.exit()
 
-_collector = DB.FilteredElementCollector(doc).OfCategory(DB.BuiltInCategory.OST_StructuralColumns).WhereElementIsNotElementType()
+base_locations = [get_bounding_box_center(element) for element in selected_elements]
 
-selSet = []
+collector = FilteredElementCollector(doc) \
+    .OfCategory(BuiltInCategory.OST_StructuralColumns) \
+    .WhereElementIsNotElementType()
 
-for _col in _collector:
-    if point_close(bbmid(_col),baselocation):
-        selSet.append(_col)
+selected_columns = []
 
-revit.get_selection().set_to(selSet)
+for column in collector:
+    column_center = get_bounding_box_center(column)
+    if any(is_point_close(column_center, base_location) for base_location in base_locations):
+        selected_columns.append(column)
+
+revit.get_selection().set_to(selected_columns)
