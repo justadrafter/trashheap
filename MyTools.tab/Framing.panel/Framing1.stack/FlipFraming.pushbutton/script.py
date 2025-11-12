790384d2bc938a75fac3081c4bc2f03d80d6602c
justadrafter <adam.shaw@hera.net.au>
2025-11-12 09:39:20 +0800

    updated framing ends swap

--- MyTools.tab/Framing.panel/Framing1.stack/FlipFraming.pushbutton/script.py
+++ MyTools.tab/Framing.panel/Framing1.stack/FlipFraming.pushbutton/script.py
@@ -4,24 +4,37 @@ __title__ = "Flip Framing"
 __author__ = "Adam Shaw"
 
 SCRIPT_OUTPUT = script.get_output()
-
 doc = revit.doc
+
 
 def flip_framing_ends():
     # Get the current selection
     selection = revit.get_selection()
-    results = []
+    failed_elements = []
+
     # Start a transaction
     with revit.Transaction("Flip Framing Ends"):
         for element in selection:
             # Check if the element is a structural framing
-            if isinstance(element, DB.FamilyInstance) and element.Category.Id == DB.ElementId(DB.BuiltInCategory.OST_StructuralFraming):
-                results.append(element.flipFacing())
-        if False in results:
-            print("Failed to flip these members: ")
-            for result in results:
-                if not result:
-                    print(SCRIPT_OUTPUT.linkify(element.Id))
+            if isinstance(
+                element, DB.FamilyInstance
+            ) and element.Category.Id == DB.ElementId(
+                DB.BuiltInCategory.OST_StructuralFraming
+            ):
+                try:
+                    # Check if ends can be flipped
+                    if DB.Structure.StructuralFramingUtils.CanFlipEnds(element):
+                        DB.Structure.StructuralFramingUtils.FlipEnds(element)
+                    else:
+                        failed_elements.append(element)
+                except Exception as e:
+                    failed_elements.append(element)
+
+    if failed_elements:
+        print("Failed to flip framing members: {}".format(
+            ", ".join([SCRIPT_OUTPUT.linkify(elem.Id) for elem in failed_elements])
+        ))
+
 
 if __name__ == "__main__":
     flip_framing_ends()
